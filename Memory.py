51de3dbb993f7057ebc7079092dcf1b22a92968c
Dreary <drearyland22021@gmail.com>
2023-09-15 12:44:25 -0600

    Added tap counting feature and comments for clarity

--- Memory.py
+++ Memory.py
@@ -1,27 +1,29 @@
-"""Memory, puzzle game of number pairs.
+"""Memory, a puzzle game of number pairs.
 
 Exercises:
 
 1. Count and print how many taps occur.
 2. Decrease the number of tiles to a 4x4 grid.
 3. Detect when all tiles are revealed.
-4. Center single-digit tile.
+4. Center single-digit tiles.
 5. Use letters instead of tiles.
 """
 
 from random import *
 from turtle import *
-
 from freegames import path
 
+# Load the car image
 car = path('car.gif')
+
+# Create a list of number pairs for the tiles
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
+tap_count = 0  # Initialize the tap count
 
-
+# Function to draw a white square with a black outline at (x, y)
 def square(x, y):
-    """Draw white square with black outline at (x, y)."""
     up()
     goto(x, y)
     down()
@@ -32,19 +34,18 @@ def square(x, y):
         left(90)
     end_fill()
 
-
+# Function to convert (x, y) coordinates to a tile index
 def index(x, y):
-    """Convert (x, y) coordinates to tiles index."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
-
+# Function to convert a tile count to (x, y) coordinates
 def xy(count):
-    """Convert tiles count to (x, y) coordinates."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
-
+# Function to handle tile taps and update the game state
 def tap(x, y):
-    """Update mark and hidden tiles based on tap."""
+    global tap_count  # Declare tap_count as a global variable
+
     spot = index(x, y)
     mark = state['mark']
 
@@ -55,9 +56,10 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
+    tap_count += 1  # Increment the tap count
 
+# Function to draw the game board
 def draw():
-    """Draw image and tiles."""
     clear()
     goto(0, 0)
     shape(car)
@@ -77,15 +79,29 @@ def draw():
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
+    # Display the tap count on the screen
+    up()
+    goto(-180, -180)  # Adjust the position as needed
+    color('black')
+    write(f'Taps: {tap_count}', font=('Arial', 16, 'normal'))
+
     update()
     ontimer(draw, 100)
 
-
+# Shuffle the tiles to randomize their positions
 shuffle(tiles)
+
+# Set up the turtle window
 setup(420, 420, 370, 0)
 addshape(car)
 hideturtle()
 tracer(False)
+
+# Register the tap function to handle mouse clicks
 onscreenclick(tap)
+
+# Start the game loop by calling the draw function
 draw()
+
+# Finish the game
 done()
